1aa4677a2bd4cc0c4c5202f371cc2e4ab6fd2afa
sirAlexander <alexander.shikanga.tindi@gmail.com>
2020-04-23 22:01:44 +0100

    added select list, and the ability to switch currency

--- bitzprice/components/Prices.js
+++ bitzprice/components/Prices.js
@@ -1,22 +1,56 @@
 class Prices extends React.Component {
 
     state = {
-        currency: 'USD'
+        currency: 'GBP'
     }
 
-    render(){
+    render() {
+
+        let list = '';
+
+        if (this.state.currency === 'GBP') {
+            list = <li className="list-group-item">
+                Bitcoin rate for {this.props.bpi.GBP.description}: &nbsp;
+            <span className="badge badge-primary">
+                    {this.props.bpi.GBP.code}
+                </span>
+                <strong>&nbsp; {this.props.bpi.USD.rate}</strong>
+            </li>
+
+        } else if (this.state.currency === 'EUR') {
+            list = <li className="list-group-item">
+                Bitcoin rate for {this.props.bpi.EUR.description}: &nbsp;
+            <span className="badge badge-primary">
+                    {this.props.bpi.EUR.code}
+                </span>
+                <strong>&nbsp; {this.props.bpi.USD.rate}</strong>
+            </li>
+
+        } else if (this.state.currency === 'USD') {
+            list = <li className="list-group-item">
+                Bitcoin rate for {this.props.bpi.USD.description}: &nbsp;
+            <span className="badge badge-primary">
+                    {this.props.bpi.USD.code}
+                </span>
+                <strong>&nbsp; {this.props.bpi.USD.rate}</strong>
+            </li>
+
+        }
+
+
         return (
-        <div>
-            <ul className="list-group">
-                <li className="list-group-item">
-                    Bitcoin rate for {this.props.bpi.USD.description}: &nbsp; 
-                    <span className="badge badge-primary">
-                        {this.props.bpi.USD.code}
-                    </span>
-                    <strong>&nbsp; {this.props.bpi.USD.rate}</strong>
-                </li>
-            </ul>
-        </div>
+            <div>
+                <ul className="list-group">
+                    {list}
+                </ul>
+                <br />
+                <select onChange={e => this.setState({ currency: e.target.value })}
+                    className="form-control">
+                        <option value="GBP">GBP</option>
+                        <option value="EUR">EUR</option>
+                        <option value="USD">USD</option>
+                </select>
+            </div>
         );
     }
 }
